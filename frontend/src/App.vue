b41df71131464a15d23a2adb5105b6017b72035e
roma0323 <r911223oger@gmail.com>
2022-05-10 22:40:29 +0800

    before_tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,22 @@
 <template>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>blog</title>
+</head>
+<body>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+</body>
+</html>
+  
 </template>
 
 <style>
