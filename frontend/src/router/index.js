7148352861242cb5b4737043c5caed1da1ef5c17
roma0323 <r911223oger@gmail.com>
2022-05-10 22:40:08 +0800

    before_tailwind

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,9 +10,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    // 有點進去才加載
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
